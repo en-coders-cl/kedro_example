aef94003efdc28320d35af3f62307b0c0f23e6b2
Rodo-ASUS <rununez@entel.cl>
2022-12-13 04:24:22 -0300

    extending data preprocessing pipeline to get a merged table

--- src/kedro_example/pipelines/data_processing/pipeline.py
+++ src/kedro_example/pipelines/data_processing/pipeline.py
@@ -4,7 +4,8 @@ generated using Kedro 0.18.4
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import preprocess_companies, preprocess_shuttles
+from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
@@ -20,6 +21,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs="shuttles",
                 outputs="preprocessed_shuttles",
                 name="preprocess_shuttles_node",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
+                outputs="model_input_table",
+                name="create_model_input_table_node",
             ),
         ]
     )
